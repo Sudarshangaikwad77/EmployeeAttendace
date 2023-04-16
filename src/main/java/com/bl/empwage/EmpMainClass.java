0404495caee1fedee64e1ef5b205218427815f3d
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-04-17 00:20:21 +0530

    added code to check employee Wage

--- src/main/java/com/bl/empwage/EmpMainClass.java
+++ src/main/java/com/bl/empwage/EmpMainClass.java
@@ -4,11 +4,22 @@ public class EmpMainClass
 {
     public static void main(String[] args)
     {
-        System.out.println("welcome to employee wage project");
-        int attendance = (int) (Math.random() *10) %2;
-        if (attendance == 1)
-            System.out.println("employee is present ");
+       //constant
+        int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HR=20;
+        //Variables
+        int emp_hr=0;
+        int empWage=0;
+        //computation
+        double empcheck=Math.floor(Math.random() * 10) %2;
+        if (empcheck ==IS_FULL_TIME)
+            emp_hr=8;
         else
-            System.out.println("employee is absent");
+            emp_hr=0;
+        empWage=emp_hr * EMP_RATE_PER_HR;
+        System.out.println("emp_wage : "+empWage);
+
     }
+
+
 }
