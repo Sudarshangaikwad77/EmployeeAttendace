e72e419d0eb8241fb9730bf1705e3bf92b1dd4a3
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-04-21 05:50:01 +0530

    code added to uc-4

--- src/main/java/com/bl/employee_wage2/Emp_wage.java
+++ src/main/java/com/bl/employee_wage2/Emp_wage.java
@@ -1,7 +1,9 @@
 package com.bl.employee_wage2;
 
 public class Emp_wage
-{
+{    public static final int Part_time =1;
+    public static final int Full_time =2;
+
     public static void main(String[] args) {
 
     System.out.println("Welcome to Employee Wage Computation");
@@ -12,5 +14,41 @@ public class Emp_wage
         }else{
             System.out.println("Employee is Absent");
         }
-}
+
+        //Calculating Wage Per Hour UC-2
+
+        int hour_wage= 20;
+        int full_day=8;
+
+        int wage = (hour_wage * full_day);
+
+        System.out.println("Employee Daily wage is: "+ wage);
+
+        //Calculating PartTime UC-3
+
+
+
+        int Emp_Hour=0;
+        int Emp_wage=0;
+
+        int EmpCheck =(int) Math.floor(Math.random()* 10) % 3;
+
+        //Using Switch Cases UC-4
+
+        switch (EmpCheck){
+            case Part_time:
+                Emp_Hour = 4;
+                break;
+            case Full_time:
+                Emp_Hour = 8;
+                break;
+            default:
+                Emp_Hour = 0;
+        }
+        Emp_wage = Emp_Hour * hour_wage;
+        System.out.println("Your Wage: "+Emp_wage);
+
+
+
+    }
 }
